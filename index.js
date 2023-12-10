dbefabae5a69f424f46bc7caf45a4f6df40a910c
Efrencima <149048205+Efrencima@users.noreply.github.com>
2023-12-10 23:17:26 +0800

    index.js

--- index.js
+++ index.js
@@ -1,99 +1,67 @@
-const gridContainer = document.querySelector(".grid-container");
-let cards = [];
-let firstCard, secondCard;
-let lockBoard = false;
-let score = 0;
+let counter = 0;
+let firstSelection = "";
+let secondSelection = "";
 
-document.querySelector(".score").textContent = score;
+const cards = document.querySelectorAll(".cards .card");
+const playAgainButton = document.getElementById("play-again");
 
-fetch("./data/cards.json")
-  .then((res) => res.json())
-  .then((data) => {
-    cards = [...data, ...data];
-    shuffleCards();
-    generateCards();
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
+        if (counter < 2 && !card.classList.contains("checked") && !card.classList.contains("clicked")) {
+            card.classList.add("clicked");
+
+            if (counter === 0) {
+                firstSelection = card.getAttribute("animal");
+                counter++;
+            } else {
+                secondSelection = card.getAttribute("animal");
+                counter = 0;
+
+                if (firstSelection === secondSelection) {
+                    const correctCards = document.querySelectorAll(
+                        ".card[animal='" + firstSelection + "']:not(.checked)"
+                    );
+
+                    correctCards.forEach((correctCard) => {
+                        correctCard.classList.add("checked");
+                        correctCard.classList.remove("clicked");
+                    });
+
+                    if (document.querySelectorAll(".card:not(.checked)").length === 0) {
+                        playAgainButton.style.display = "block";
+                    }
+                } else {
+                    const incorrectCards = document.querySelectorAll(".card.clicked");
+
+                    incorrectCards.forEach((incorrectCard) => {
+                        incorrectCard.classList.add("shake");
+                    });
+
+                    setTimeout(() => {
+                        incorrectCards.forEach((incorrectCard) => {
+                            incorrectCard.classList.remove("shake");
+                            incorrectCard.classList.remove("clicked");
+                        });
+                    }, 800);
+                }
+            }
+        }
+    });
+});
+
+playAgainButton.addEventListener("click", () => {
+  cards.forEach((card) => {
+      card.classList.remove("checked", "clicked", "shake");
   });
 
-function shuffleCards() {
-  let currentIndex = cards.length,
-    randomIndex,
-    temporaryValue;
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = cards[currentIndex];
-    cards[currentIndex] = cards[randomIndex];
-    cards[randomIndex] = temporaryValue;
-  }
-}
-
-function generateCards() {
-  for (let card of cards) {
-    const cardElement = document.createElement("div");
-    cardElement.classList.add("card");
-    cardElement.setAttribute("data-name", card.name);
-    cardElement.innerHTML = `
-      <div class="front">
-        <img class="front-image" src=${card.image} />
-      </div>
-      <div class="back"></div>
-    `;
-    gridContainer.appendChild(cardElement);
-    cardElement.addEventListener("click", flipCard);
-  }
-}
-
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-
-  this.classList.add("flipped");
-
-  if (!firstCard) {
-    firstCard = this;
-    return;
-  }
-
-  secondCard = this;
-  score++;
-  document.querySelector(".score").textContent = score;
-  lockBoard = true;
-
-  checkForMatch();
-}
-
-function checkForMatch() {
-  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
-
-  isMatch ? disableCards() : unflipCards();
-}
-
-function disableCards() {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
-
-  resetBoard();
-}
-
-function unflipCards() {
-  setTimeout(() => {
-    firstCard.classList.remove("flipped");
-    secondCard.classList.remove("flipped");
-    resetBoard();
-  }, 1000);
-}
-
-function resetBoard() {
-  firstCard = null;
-  secondCard = null;
-  lockBoard = false;
-}
-
-function restart() {
-  resetBoard();
+  playAgainButton.style.display = "none";
   shuffleCards();
-  score = 0;
-  document.querySelector(".score").textContent = score;
-  gridContainer.innerHTML = "";
-  generateCards();
+});
+
+function shuffleCards() {
+  cards.forEach((card) => {
+      const randomIndex = Math.floor(Math.random() * cards.length);
+      card.style.order = randomIndex;
+  });
 }
+
